c19b203dc0fe2ccc2ffd4ee934290ab1eb637893
Vlad Popescu <vipopescu@outlook.com>
2023-11-30 10:23:42 +1100

    Refactor: Rename API endpoints for consistent styling. Add BadTags by instance operation.

--- Controllers/MXDataController.cs
+++ Controllers/MXDataController.cs
@@ -15,29 +15,8 @@ namespace MXAccesRestAPI.Controllers
             _dataHolderService = dataHolderService;
         }
 
-        [HttpGet("reference/{fullAttrName}")]
-        public IActionResult GetData(string fullAttrName)
-        {
-            MXAttribute mxattr = _dataHolderService.GetData(fullAttrName);
-            if (mxattr == null)
-                return NotFound("Data not found.");
-            return Ok(mxattr.AsDto());
-        }
-
-        [HttpPut("reference/{fullAttrName}")]
-        public IActionResult UpdateData(string fullAttrName, [FromBody] UpdadeAttributeDto updatedValue)
-        {
-            MXAttribute mxattr = _dataHolderService.GetData(fullAttrName);
-            if (mxattr == null)
-                return NotFound("Data not found.");
-
-            _dataHolderService.WriteData(fullAttrName, updatedValue.new_value, DateTime.Now);
-
-            return NoContent();
-        }
-
-        [HttpGet("instance/{instanceName}")]
-        public IActionResult GetDataByInstance(string instanceName)
+        [HttpGet("instances/{instanceName}/tags")]
+        public IActionResult GetInstanceData(string instanceName)
         {
             if (string.IsNullOrEmpty(instanceName)) return NotFound("Data not found.");
             var data = _dataHolderService.GetInstanceData(instanceName)
@@ -47,7 +26,40 @@ namespace MXAccesRestAPI.Controllers
             return Ok(data);
         }
 
-        [HttpGet("baddata")]
+        [HttpGet("instances/{instanceName}/tags/{tagId}")]
+        public IActionResult GetTagData(string instanceName, string tagId)
+        {
+            string fullAttrName = instanceName + "." + tagId;
+            MXAttribute mxattr = _dataHolderService.GetData(fullAttrName);
+            if (mxattr == null)
+                return NotFound("Data not found.");
+            return Ok(mxattr.AsDto());
+        }
+
+        [HttpPut("instances/{instanceName}/tags/{tagId}")]
+        public IActionResult UpdateData(string instanceName, string tagId, [FromBody] UpdadeAttributeDto updatedValue)
+        {
+            string fullAttrName = instanceName + "." + tagId;
+            MXAttribute mxattr = _dataHolderService.GetData(fullAttrName);
+            if (mxattr == null)
+                return NotFound("Data not found.");
+
+            _dataHolderService.WriteData(fullAttrName, updatedValue.new_value, DateTime.Now);
+
+            return NoContent();
+        }
+
+        [HttpGet("instances/{instanceName}/bad-tags")]
+        public IActionResult GetBadAndUncertainTagsForInstance(string instanceName)
+        {
+            var data = _dataHolderService.GetBadAndUncertainData(instanceName)
+                                    .Select(x => x.AsDto());
+            if (data == null)
+                return NotFound("Data not found.");
+            return Ok(data);
+        }
+
+        [HttpGet("instances/bad-tags")]
         public IActionResult GetBadAndUncertainTags()
         {
             var data = _dataHolderService.GetBadAndUncertainData()
@@ -57,7 +69,7 @@ namespace MXAccesRestAPI.Controllers
             return Ok(data);
         }
 
-        [HttpGet("alldata")]
+        [HttpGet("instances/all")]
         public IActionResult GetAllData()
         {
             var data = _dataHolderService.GetAllData()
