155c66057649dfb2d57f41ab7bfa7c9a56256891
Vlad Popescu <vipopescu@outlook.com>
2024-01-22 12:11:57 +1100

    Set to 2 the default to MX Data Service Threads to avoid startup crash.

--- Settings/MxDataDataServiceSettings.cs
+++ Settings/MxDataDataServiceSettings.cs
@@ -5,7 +5,7 @@ namespace MXAccesRestAPI.Settings
         public string ServerName { get; init; }
         public string LmxVerifyUser { get; init; }
 
-        public int MxDataServiceThreads { get; init; }
+        public int MxDataServiceThreads { get; init; } = 2;
 
     }
 }
